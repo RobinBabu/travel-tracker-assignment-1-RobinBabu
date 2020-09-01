1bc66e48d393042c21d0fa6beb946c7715f3cb2d
RobinBabu <robin.babu@my.jcu.edu.au>
2020-09-01 20:54:49 +1000

    Starting of assignment.

--- assignment1.py
+++ assignment1.py
@@ -1,13 +1,20 @@
 """
 Replace the contents of this module docstring with your own details
-Name:
-Date started:
-GitHub URL:
+Name: Robin Babu
+Date started: 1/09/2020
+GitHub URL: https://github.com/RobinBabu
 """
 
 
 def main():
-    print("Travel Tracker 1.0 - by <Your Name>")
+    print("Welcome Travel Tracker 1.0 - by Robin Babu")
+
+    print(" Menu                         \n    "
+          " L - List places              \n    "
+          " A - Add a new place          \n    "
+          " M - Mark a place as visited  \n    "
+          " Q - Quit")
+
 
 
 if __name__ == '__main__':
