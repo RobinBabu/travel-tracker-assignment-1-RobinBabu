26311eb548e47353e0ca06143083ad47685d11f2
RobinBabu <robin.babu@my.jcu.edu.au>
2020-09-03 02:49:30 +1000

    Add menu options and load file.

--- assignment1.py
+++ assignment1.py
@@ -4,14 +4,13 @@ Name: Robin Babu
 Date started: 1/09/2020
 GitHub URL: https://github.com/RobinBabu
 """
-
-places_details = []
-places_file = open('places.csv')
+import csv
+from operator import itemgetter
 
 
 def main():
     print("Welcome Travel Tracker 1.0 - by Robin Babu")
-
+    list_of_places = load_original_file()
     choice = input("Menu: \n"
                    "L - List places \n"
                    "A - Add new place \n"
@@ -19,32 +18,39 @@ def main():
                    "Q - Quit \n"
                    ">>>").upper()
 
+    while choice != "Q":
+        if choice == "L":
+            print_list_places(list_of_places)
+        elif choice == "A":
+            add_new_place(list_of_places)
+        elif choice == "M":
+            mark_visited(list_of_places)
+        elif choice == "":
+            print("Input cannot be blank")
+        else:
+            print("Invalid menu choice")
+
+        choice = input("Menu: \nL - List places \nA - Add new place \nM - Mark a place as visited \nQ - Quit "
+                       "\n>>> ").upper()
+
+    with open('places.csv', 'w', newline='') as resultFile:
+        writer = csv.writer(resultFile)
+        writer.writerows(list_of_places)
+    print('{} places saved to places.csv\nHave a nice day :) '.format(len(list_of_places)))
 
 
-while choice != "Q":
-    if choice == "L":
-        places_file = open('places.csv', 'w')
-
-
-
-
-"""
-while choice != "Q":
-    if choice == "L":
-        print()
-    else if choice == <second option>
-        <do second task>
-    ...
-    else if choice == <n-th option>
-        <do n-th task>
-    else
-        display invalid input error message
-    display menu
-    get choice
-<do final thing, if needed>
-
-"""
-
+# Function Used to Open Initial Places File.
+def load_original_file():
+    file_places = open("places.csv", "r")
+    reader = csv.reader(file_places, delimiter=',')
+    places = list(reader)
+    print('{} places loaded from places.csv'.format(len(places)))
+    # String to integer conversion
+    for place in places:
+        place[1] = int(place[1])
+    file_places.close()
+    places.sort(key=itemgetter(1, 0))
+    return places
 
 
 if __name__ == '__main__':
