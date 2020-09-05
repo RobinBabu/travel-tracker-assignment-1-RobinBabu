039c069e5331deba4ea2ebd65dc51f06dcf5f30f
RobinBabu <robin.babu@my.jcu.edu.au>
2020-09-05 16:27:09 +1000

    Load list of places from places.csv

--- assignment1.py
+++ assignment1.py
@@ -4,20 +4,24 @@ Name: Robin Babu
 Date started: 1/09/2020
 GitHub URL: https://github.com/RobinBabu
 """
-
-places_file = open('places.csv', 'r')
-valid_input = False
-place_details = []
+from operator import itemgetter
 
 
 def main():
     """Travel Tracker program allows a user to track places they wish to visit and places they have already visited"""
+    valid_input = False
+    place_details = []
+    places_file = open('places.csv', 'r')
+
+    get_list_of_lists(place_details, places_file)
     print("Welcome Travel Tracker 1.0 - by Robin Babu")
+    print("{} places loaded from places.csv".format(len(place_details)))
+
     choice = get_choice()
 
     while choice != "Q":
         if choice == "L":
-            print('List test')
+            print_list(place_details)
             return
         elif choice == "A":
             open('places.csv', 'w')
@@ -31,6 +35,15 @@ def main():
         choice = get_choice()
 
     print("{} places saved to places.csv \nHave a nice day! :)".format(len(place_details)))
+    places_file.close()
+
+
+# sorts the places.csv file into a list of lists, returns the list of lists
+def get_list_of_lists(place_details, places_file):
+    for place in places_file:
+        place = place.strip().split(',')
+        place_details.append([place[0], (place[1]), place[2], place[3]])
+    place_details.sort(key=itemgetter(1))
 
 
 def get_choice():
@@ -50,6 +63,20 @@ def get_choice():
                        " Q - Quit \n"
                        ">>>").upper()
     return choice
+
+
+#  (choice == L) prints the list of lists contents,
+def print_list(place_details):
+    unvisited_count = 0
+    place_details.sort(key=itemgetter(1, 0))
+    for i, place in enumerate(place_details):
+        if place[3] == 'v':
+            unvisited_count += 1
+            visited_status = ' '
+        else:
+            visited_status = '*'
+        print('{}{:2}. {:20} in {:<20} priority {:>5}'.format(visited_status, i, place[0], place[1], place[2]))
+    print('{} places. You still want to visit {} places. '.format(unvisited_count, len(place_details) - unvisited_count))
 
 
 if __name__ == '__main__':
