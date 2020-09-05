bce73b18c96b1a562ecf86a4c20af70f8abb5261
RobinBabu <robin.babu@my.jcu.edu.au>
2020-09-05 19:27:23 +1000

    Add new place and save place into file.

--- assignment1.py
+++ assignment1.py
@@ -11,7 +11,7 @@ def main():
     """Travel Tracker program allows a user to track places they wish to visit and places they have already visited"""
     valid_input = False
     place_details = []
-    places_file = open('places.csv', 'r')
+    places_file = open('places.csv', 'r')   # Loads file
 
     get_list_of_lists(place_details, places_file)
     print("Welcome Travel Tracker 1.0 - by Robin Babu")
@@ -22,24 +22,30 @@ def main():
     while choice != "Q":
         if choice == "L":
             print_list(place_details)
-            return
         elif choice == "A":
             open('places.csv', 'w')
-            print('Add new place test')
-            return
+            while not valid_input:
+                try:
+                    add_new_place(place_details)
+                    valid_input = True
+                except ValueError:
+                    print("Invalid input,")
 
         elif choice == "M":
             print('Mark place as visited test ')
             return
+
         print("")
         choice = get_choice()
-
+        valid_input = False
+        print("")
+    save_places(place_details)
     print("{} places saved to places.csv \nHave a nice day! :)".format(len(place_details)))
     places_file.close()
 
 
-# sorts the places.csv file into a list of lists, returns the list of lists
 def get_list_of_lists(place_details, places_file):
+    """Stores place data in list of lists and returns list when called."""
     for place in places_file:
         place = place.strip().split(',')
         place_details.append([place[0], (place[1]), place[2], place[3]])
@@ -54,19 +60,20 @@ def get_choice():
                    "M - Mark a place as visited \n"
                    "Q - Quit \n"
                    ">>>").upper()
+
     while choice not in ("L", "A", "M", "Q"):
-        print("Invalid menu choice")
+        print("Invalid menu choice, please enter a valid menu option.")
         choice = input("Menu: \n"
                        " L - List place \n"
                        " A - Add a new place \n"
-                       " M - Mark place as visited \n"
+                       " M - Mark a place as visited \n"
                        " Q - Quit \n"
                        ">>>").upper()
     return choice
 
 
-#  (choice == L) prints the list of lists contents,
 def print_list(place_details):
+    """Prints list contents from places.csv displaying visited or unvisited status."""
     unvisited_count = 0
     place_details.sort(key=itemgetter(1, 0))
     for i, place in enumerate(place_details):
@@ -76,7 +83,48 @@ def print_list(place_details):
         else:
             visited_status = '*'
         print('{}{:2}. {:20} in {:<20} priority {:>5}'.format(visited_status, i, place[0], place[1], place[2]))
-    print('{} places. You still want to visit {} places. '.format(unvisited_count, len(place_details) - unvisited_count))
+    print('{} places. You still want to visit {} places. '.format(unvisited_count, len(place_details) -
+                                                                  unvisited_count))
+
+
+def check_string(prompt):
+    """Function to error check if string is being entered by user."""
+    string_check = input(prompt)
+    while string_check == '':
+        print('Input cannot be blank')
+        string_check = input(prompt)
+    return string_check
+
+
+def check_integer(prompt):
+    """Function to error check if integer value is being entered by user."""
+    valid_input = False
+    while not valid_input:
+        try:
+            value = int(input(prompt))
+            if int(value) >= 0:
+                valid_input = True
+                return value
+            else:
+                print('Number must be > 0')
+        except ValueError:
+            print('Invalid input, please enter a valid number')
+
+
+def add_new_place(place_details):
+    """Add new place to places.csv"""
+    place_name = check_string('Name: ')
+    place_country = check_string('Country: ')
+    place_priority = check_integer('Priority: ')
+    place_details.append([place_name, place_country, place_priority, 'u'])
+    print('{} in {} (priority {}) added to Travel Tracker'.format(place_name, place_country, place_priority))
+
+
+def save_places(place_details):
+    """Saves added place into places.csv before closing file."""
+    places_file = open('places.csv', 'w')
+    for i in place_details:
+        places_file.writelines("{},{},{},{}\n".format(i[0], i[1], i[2], i[3]))
 
 
 if __name__ == '__main__':
